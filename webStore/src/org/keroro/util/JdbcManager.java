1557dc6b7b43e9204c0b9d83f27c2393f385db2c
crobbitkent <crobbitkent@gmail.com>
2021-01-23 00:28:21 +0900

    21/01/23 - webStudy

--- webStore/src/org/keroro/util/JdbcManager.java
+++
@@ -1,8 +0,0 @@
-package org.keroro.util;
-
-public class JdbcManager {
-	public final static String URL = "jdbc:oracle:thin:@112.169.196.210:1521:XE";
-	public final static String USER_ID = "mingyu";
-	public final static String USER_PW = "alsrb";
-	public final static String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-}
